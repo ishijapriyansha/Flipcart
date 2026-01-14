ccfb1866068339121ff749bd65b90795c5407ae3
Lenovo <ishijapriyansha19@gmail.com>
2026-01-14 20:23:25 +0530

    cartContext debugging

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -15,25 +15,25 @@ export function CartProvider({children}){
                 })
                 return newCart
            }
-          
-           return newCart
+           return [...prev, {...prod, quantity:1}]
         })
     }
 
-    function reduceFromCart(prod){
-        setCart((prev)=>{
-        const existing= prev.find((item)=>item.id===prod.id)
-           if(existing){
-            
-                const newCart= prev.map((item)=>{
-                    if(item.id===prod.id) return {...item, quantity: item.quantity -1 }
-                    return item
-                })
-           }
-           return newCart
-        })
-    }
+    function reduceFromCart(prod) {
+        setCart((prev) => {
+        const existing = prev.find((item) => item.id === prod.id);
 
+        if (!existing) return prev
+        if (existing.quantity === 1) return prev.filter((item) => item.id !== prod.id);
+    
+        const newCart = prev.map((item) => {
+            if (item.id === prod.id) return { ...item, quantity: item.quantity - 1 };
+            return item;
+        });
+
+        return newCart;
+  });
+}
     const totalPrice=cart.reduce((acc, curr)=> acc+ curr.price * curr.quantity, 0)
 
     return (
